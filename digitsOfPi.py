d0b378798e365d79d31a54973c5223cc952cf332
bhanni01 <114847745+bhanni01@users.noreply.github.com>
2024-07-30 13:34:11 -0500

    trying to name the dots so i can use it as starting and closing points to connect the dot

--- digitsOfPi.py
+++ digitsOfPi.py
@@ -8,11 +8,17 @@ class piArt(Scene):
         numPoints = 10
         # lets try to create point in the circle 
         # each point should be created 36 degrees apart to each other , pi/10
-
+        points ={}
         for point in range(numPoints):
-            point = circle.point_at_angle(point*(PI/5))
-            dot = Circle(radius=0.05, color=WHITE).move_to(point)
+            points["point{0}".format(point)]= circle.point_at_angle(point*(PI/5)) 
+            
+            # point = circle.point_at_angle(point*(PI/5))
+            dot = Circle(radius=0.05, color=WHITE).move_to("point{0}".format(point))
             self.add(circle, dot)
+        print(points)
+# need to label those points in the circles from 0-9
+# best way might be to create a dictionary and store all those commands in a dictionary
+        
 
-
-
+# ask for user how many pi decimals input they want
+# get the value and store it and start connecting dots
